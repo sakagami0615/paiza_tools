9f4054fc1a29c90a2eef8c72ab9c493d3d2bd0c1
sakagami0615 <sakagami0615@gmail.com>
2022-08-18 02:57:54 +0900

    フォーマッタによる整形を実施

--- tools/common/file_exist_checker.py
+++ tools/common/file_exist_checker.py
@@ -9,9 +9,11 @@ def check_file_exist(filepath: str) -> None:
     if os.path.isfile(filepath):
         raise FileExistsError
 
+
 def check_file_not_exist(filepath: str) -> None:
     if not os.path.isfile(filepath):
         raise FileNotExistsError
+
 
 def check_dir_exist(dirpath: str) -> None:
     if os.path.isdir(dirpath):
